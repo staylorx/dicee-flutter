b114e444eda47c29782726dbfe00da50377c5e1b
Steve Taylor <staylorx@gmail.com>
2022-09-07 07:04:12 -0600

    updated to latest flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+//because we only have six images
+const int sideCount = 6;
 
 void main() {
   return runApp(
@@ -6,18 +11,61 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDieNumber = 1;
+  int rightDieNumber = 1;
+  Random random = Random(DateTime.now().millisecondsSinceEpoch);
+
+  randomDieRoll() {
+    return random.nextInt(sideCount) + 1;
+  }
+
+  void changeDieFace() {
+    setState(() {
+      leftDieNumber = randomDieRoll();
+      rightDieNumber = randomDieRoll();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDieFace();
+              },
+              child: Image.asset('images/dice$leftDieNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDieFace();
+              },
+              child: Image.asset('images/dice$rightDieNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
